e23fc80d53b9f6eddb2440533f246b642cb24e8f
Lakshmi <lakshmichawan1@gmail.com>
2025-10-22 09:40:19 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('login.html')  # your username/password form
+
+@app.route('/result', methods=['POST'])
+def result():
+    username = request.form['username']
+    password = request.form['password']
+    return render_template('result.html', username=username, password=password)
+
+if __name__ == '__main__':
+    app.run(debug=True)
